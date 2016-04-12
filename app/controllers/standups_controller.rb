f8a4a66a234d27273f17f67172a82d8add7bf221
Charlotte Voskuilen <Charlottevoskuilen@hotmail.com>
2016-04-12 09:09:27 +0200

    Deleted some code I didn't need

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -1,52 +1,53 @@
 class StandupsController < ApplicationController
-  before_action :require_user, only: [:index, :show, :new, :edit, :create, :update, :destroy, :private]
-  
+  before_action :require_user
+
   def index
     @standups = Standup.where(user_id: current_user.id).all
   end
-  
+
   def show
     @standup = Standup.find(params[:id])
   end
 
-  def new 
+  def new
     @standup = Standup.new
     @team = Team.where(user_id: current_user.id).all
   end
-  
+
   def edit
     @standup = Standup.find(params[:id])
     @team = Team.where(user_id: current_user.id).all
   end
-  
+
   def create
     @standup = Standup.new(standup_params)
     @standup.user_id = current_user.id
-    
+
     if @standup.save
       redirect_to @standup
     else
+      @team = Team.where(user_id: current_user.id).all
       render 'new'
     end
   end
-  
+
   def update
     @standup = Standup.find(params[:id])
-    
+
     if @standup.update(standup_params)
       redirect_to @standup
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     @standup = Standup.find(params[:id])
     @standup.destroy
-    
+
     redirect_to standups_path
   end
-  
+
   private
     def standup_params
       params.require(:standup).permit(:yesterday, :today, :problems, :team_id)
